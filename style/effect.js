f2b50e9a2a87540e72b304be45cc48fc5ceace20
FreakLign <32242776+FreakLign@users.noreply.github.com>
2023-03-21 21:20:51 +0800

    a

--- style/effect.js
+++ style/effect.js
@@ -3,5 +3,5 @@ setInterval(()=>{
     var startTime = new Date("2023/02/14 00:00");
     var st = time - startTime;
     document.getElementById("timenow").innerText = 
-     "我们相爱了" + parseInt(st / 1000 / 60 / 60 / 24+1).toString() + "天呐~~";
+     "我们相爱了" + parseInt(st / 1000 / 60 / 60 / 24+1).toString() + "天呐";
 },500);
